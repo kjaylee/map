a0767282761add74f59df2d7be15300e10ae9aea
Jay Lee <k.jaylee@gmail.com>
2022-05-27 16:43:43 +0900

    Initial Commit

---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by DEV on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
